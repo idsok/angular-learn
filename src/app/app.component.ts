aaedbf08289924dc58fa8c085974b3bd583302e1
Idrissa SOKHONA <i.sokhona84@gmail.com>
2021-01-19 09:55:29 +0100

    Apprentissage de création de projet et la structure générée par CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isAuth: boolean = false;
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      },
+      4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
